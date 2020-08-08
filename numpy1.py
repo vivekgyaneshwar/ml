f4c47fa6732830eede4ae7f4de5a5acb30e34703
vivekgyaneshwar <vivekgyaneshwar@gmail.com>
2020-08-08 11:02:40 +0530

    Update numpy1.py

--- numpy1.py
+++ numpy1.py
@@ -19,3 +19,12 @@ b
 #array([6, 7, 8])
 type(b)
 #<class 'numpy.ndarray'>
+
+a = np.array([2,3,4])
+a
+#array([2, 3, 4])
+a.dtype
+#dtype('int64')
+b = np.array([1.2, 3.5, 5.1])
+#b.dtype
+dtype('float64')
