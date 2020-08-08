ed84cd23ad17ed430919868a74907d89355a78de
vivek <vivekgyaneshwar@gmail.com>
2020-08-08 13:55:25 +0530

    numpy

--- numpy1.py
+++ numpy1.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numpy import pi
+
 a = np.arange(15).reshape(3, 5)
 a
 #array([[ 0,  1,  2,  3,  4],   [ 5,  6,  7,  8,  9],     [10, 11, 12, 13, 14]])
@@ -39,3 +41,18 @@ print(np.arange( 10, 30, 5 ))
 #array([10, 15, 20, 25])
 print(np.arange( 0, 2, 0.3 ) )              # it accepts float arguments
 #array([0. , 0.3, 0.6, 0.9, 1.2, 1.5, 1.8])
+
+np.linspace( 0, 2, 9 )                 # 9 numbers from 0 to 2
+#array([0.  , 0.25, 0.5 , 0.75, 1.  , 1.25, 1.5 , 1.75, 2.  ])
+x = np.linspace( 0, 2*pi, 100 )        # useful to evaluate function at lots of points
+f = np.sin(x)
+
+
+
+print(a)
+
+b = np.arange(12).reshape(4,3)           # 2d array
+print(b)
+
+c = np.arange(24).reshape(2,3,4)         # 3d array
+print(c)
